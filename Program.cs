d5cd9cd3a3bc3f24d2870b657879453595246c53
TomaszZukowski <tomaszzukowskibp@gmail.com>
2024-01-07 10:49:08 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
